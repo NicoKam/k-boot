0c2ccc01c47e194aa776f17409b9d6dafb243222
wang.kangning <wang.kangning@alibaba-inc.com>
2021-04-07 09:00:22 +0800

    update: plugin public

--- src/config/webpack/defaultPlugins.ts
+++ src/config/webpack/defaultPlugins.ts
@@ -5,22 +5,26 @@ import HtmlPlugin from 'html-webpack-plugin';
 import type Config from 'webpack-chain';
 import webpack from 'webpack';
 import { resolve } from 'path';
+import { existsSync, readdirSync } from 'fs';
 import type { IOptions } from '../def';
 
 export default function defaultPlugin(config: Config, options: IOptions) {
   const { rootPath, srcPath, mode } = options;
   config.plugin('clean-plugin').use(CleanWebpackPlugin);
   config.plugin('html-plugin').use(HtmlPlugin, [{ template: resolve(rootPath, srcPath, 'index.html') }]);
-  config.plugin('copy-plugin').use(CopyWebpackPlugin, [
-    {
-      patterns: [
-        {
-          from: resolve(rootPath, './public'),
-          to: './',
-        },
-      ],
-    },
-  ]);
+  const publicPath = resolve(rootPath, './public');
+  if (existsSync(publicPath) && readdirSync(publicPath).length) {
+    config.plugin('copy-plugin').use(CopyWebpackPlugin, [
+      {
+        patterns: [
+          {
+            from: publicPath,
+            to: './',
+          },
+        ],
+      },
+    ]);
+  }
   if (mode === 'development') {
     config.plugin('hot-plugin').use(webpack.HotModuleReplacementPlugin);
     config.plugin('fast-refresh').use(ReactRefreshWebpackPlugin);
